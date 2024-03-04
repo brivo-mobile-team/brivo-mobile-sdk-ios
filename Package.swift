cc053134fd2835b82741b27baef42841ef2c2f5f
Thomas Prezioso <thomas.prezioso@Thomass-MacBook-Pro-2.local>
2024-03-04 15:37:27 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BrivoFluidFramework",
-            url: "https://github.com/brivo-mobile-team/Test-Brivo-Framework/blob/main/BrivoFluid.xcframework.zip",
+            url: "https://github.com/brivo-mobile-team/Test-Brivo-Framework/raw/main/BrivoFluid.xcframework.zip",
             checksum: "1ba7ea38735e19bcfbd38c2a416319f968b29e7e32bda2ef5e1f6c6df4a7d534"
         )
     ]
