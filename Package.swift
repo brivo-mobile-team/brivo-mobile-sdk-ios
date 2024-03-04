9d4ef4c1db440b5e2e444ac16372a76edd2200ab
Thomas Prezioso <thomas.prezioso@Thomass-MacBook-Pro-2.local>
2024-03-04 15:26:02 -0500

    second commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BrivoFluidFramework",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "BrivoFluidFramework",
+            targets: ["BrivoFluidFramework"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BrivoFluids",
+            url: "https://github.com/brivo-mobile-team/Test-Brivo-Framework/blob/main/BrivoFluid.xcframework.zip",
+            checksum: "1ba7ea38735e19bcfbd38c2a416319f968b29e7e32bda2ef5e1f6c6df4a7d534"
+        )
+    ]
+)
