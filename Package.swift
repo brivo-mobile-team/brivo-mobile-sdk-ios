90f8678fad04a64c78aa558fb1c6be3f07d14c96
Thomas Prezioso <thomas.prezioso@Thomass-MacBook-Pro-2.local>
2024-03-04 16:37:19 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BrivoFluidFramework",
-            targets: ["BrivoFluidFramework"]),
+            targets: ["BrivoFluidFramework","BrivoAccess","BrivoNetworkCore","BrivoBLE","BrivoCore","BrivoLocalAuthentication","BrivoOnAir"]),
     ],
     targets: [
         .binaryTarget(
