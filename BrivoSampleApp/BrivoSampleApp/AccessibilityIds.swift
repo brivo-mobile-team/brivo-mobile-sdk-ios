db2ab35d59458f246c1d44d59968d1113d2c733b
local <local@067f9ab8-6146-4b96-aa51-139576fcf04f-7dc5fbfcc8-tj6ft-vm.podset-vms.dt-skywagon-workers-prod-vm.svc.kube.us-west-2f.k8s.cloud.apple.com>
2024-07-26 21:55:25 -0700

    Update SDK Frameworks ()

---
+++ BrivoSampleApp/BrivoSampleApp/AccessibilityIds.swift
@@ -0,0 +1,14 @@
+//
+//  AccessibilityIds.swift
+//  BrivoSampleAppUITests
+//
+//  Created by Gabriel Dusa on 27.07.2024.
+//
+
+struct AccessibilityIds {
+    static let navigationPlusButton = "navigationPlusButtonId"
+    static let mobilePassEmailField = "mobilePassEmailFieldId"
+    static let mobilePassInviteCodeField = "mobilePassInviteCodeFieldId"
+    static let redeemInviteButton = "redeemInviteButtonId"
+    static let accountNameTextView = "accountNameId"
+}
