805380faeb99aa1942f5db9297917bfbc37e50fa
local <local@067f9ab8-6146-4b96-aa51-139576fcf04f-7dc5fbfcc8-t25f7-vm.podset-vms.dt-skywagon-workers-prod-vm.svc.kube.us-west-2f.k8s.cloud.apple.com>
2024-07-26 22:43:01 -0700

    Update SDK Frameworks ()

---
+++ BrivoSampleApp/BrivoSampleAppUITests/Tests/RedeemPass/GeneratePass/Models/MobilePass.swift
@@ -0,0 +1,14 @@
+//
+//  MobilePass.swift
+//  BrivoPassPlusUITests
+//
+//  Created by Gabriel Dusa on 19.04.2024.
+//
+
+struct MobilePass: Codable {
+    let id: Int
+    let referenceId: String
+    let accessCode: String
+    let status: String
+    let credentialId: Int
+}
---
+++ BrivoSampleApp/BrivoSampleAppUITests/Utilities/Extensions/String.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension String {
+    func toBase64() -> String? {
+        guard let data = self.data(using: String.Encoding.utf8) else {
+            return nil
+        }
+        return data.base64EncodedString(options: Data.Base64EncodingOptions(rawValue: 0))
+    }
+}
